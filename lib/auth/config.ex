dfef6cfe9c025230791b52af2ed0740a3c43fbb4
Maxemilian Grönblom <maxemiliang@gmail.com>
2022-01-11 23:27:04 +0200

    fix(config): use the correct naming to fetch config.

--- lib/auth/config.ex
+++ lib/auth/config.ex
@@ -1,5 +1,5 @@
 defmodule Auth0.Auth.Config do
-  defp get_env, do: Application.get_env(:auth_config, :auth0, [])
+  defp get_env, do: Application.get_env(:auth0_plug, :auth0, [])
 
   def audience, do: Keyword.get(get_env(), :audience)
 
